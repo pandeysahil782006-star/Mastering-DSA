6e2adae9a80fe8567d4908ab538bc805236875e9
Sahil Pandey <pandeysahil782006@gmail.com>
2026-07-25 01:37:22 +0530

    Solved LC 18: 4Sum (Optimal Two Pointers + Sorting)

---
+++ Arrays/Strivers/4sum.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//better
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> st; 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                
+                unordered_set<long long> hashset; 
+
+                for (int k = j + 1; k < n; k++) {
+                
+                    long long sum = (long long)nums[i] + nums[j] + nums[k];
+                    long long fourth = (long long)target - sum;
+                    if (hashset.find(fourth) != hashset.end()) {
+                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                    hashset.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+}
+
+
+//optimal
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = j + 1;
+                int l = n - 1;
+
+                while (k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum < target) {
+                        k++;
+                    } 
+                    else if (sum > target) {
+                        l--;
+                    } 
+                    else {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
