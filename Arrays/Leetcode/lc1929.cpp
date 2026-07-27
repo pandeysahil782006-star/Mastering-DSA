c6c9e8d2e7960d0c2db08a1d224e89c34388da19
Sahil Pandey <pandeysahil782006@gmail.com>
2026-07-27 20:37:53 +0530

    Solved Concatenation of Array & maintained streak

---
+++ Arrays/Leetcode/lc1929.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> getConcatenation(vector<int>& nums) {
+    vector<int> ans = nums;
+    for (int x : nums) {
+        ans.push_back(x);
+    }
+    return ans;
+}
