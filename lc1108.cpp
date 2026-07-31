7b487b72cbdc3d301fcf08bd1583f1b43157bd53
Sahil Pandey <pandeysahil782006@gmail.com>
2026-08-01 01:32:23 +0530

    Solver leetcode 1108: Defanging an IP Address

---
+++ lc1108.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string defangIPaddr(string address) {
+        string ans = "";
+        for(char c:address){
+            if(c == '.') ans+= "[.]";
+            else ans+= c;
+        }
+        return ans;
+
+}
